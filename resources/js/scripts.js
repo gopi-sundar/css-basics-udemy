cf04de5e629e5b344c29f926c3789d6c203ddf1d
Sundararaju, Gopinath <gopinath.sundararaju@pearson.com>
2019-03-07 11:40:53 -0600

    20 responsive navigation

--- resources/js/scripts.js
+++ resources/js/scripts.js
@@ -45,4 +45,18 @@ $(document).ready(function () {
     }, {
             offset: '50%'
         });
+
+    /* mobile nav */
+    $('.js--mobile-nav-icon').click(function () {
+        var nav = $('.js--main-nav');
+        var icon = $('.js--mobile-nav-icon ion-icon');
+        nav.slideToggle(200);
+        if (icon.attr('name') === 'menu') {
+            icon.attr('name', 'close')
+        }
+        else {
+            icon.attr('name', 'menu')
+        }
+
+    });
 });
