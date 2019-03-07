7d27598a8b1edd90b2301713a8a9fec7a48f6a61
Sundararaju, Gopinath <gopinath.sundararaju@pearson.com>
2019-03-07 08:05:02 -0600

    19 animations on scroll

--- resources/js/scripts.js
+++ resources/js/scripts.js
@@ -1,5 +1,6 @@
 $(document).ready(function () {
 
+    /* Sticky navigation*/
     $('.js--section-features').waypoint(function (direction) {
         if (direction == "down") {
             $('nav').addClass('sticky');
@@ -11,4 +12,37 @@ $(document).ready(function () {
             offset: '60px'
         });
 
+    /* Scroll to elements */
+    $('.js--scroll-to-plans').click(function () {
+        $('html,body').animate({ scrollTop: $('.js--section-plans').offset().top }, 1000);
+    });
+
+    $('.js--scroll-to-start').click(function () {
+        $('html,body').animate({ scrollTop: $('.js--section-features').offset().top }, 1000);
+    });
+
+    /* Animation on scroll*/
+    $('.js--wp-1').waypoint(function (direction) {
+        $('.js--wp-1').addClass('animated fadeIn');
+    }, {
+            offset: '50%'
+        });
+
+    $('.js--wp-2').waypoint(function (direction) {
+        $('.js--wp-2').addClass('animated fadeInUp');
+    }, {
+            offset: '50%'
+        });
+
+    $('.js--wp-3').waypoint(function (direction) {
+        $('.js--wp-3').addClass('animated fadeIn');
+    }, {
+            offset: '50%'
+        });
+
+    $('.js--wp-4').waypoint(function (direction) {
+        $('.js--wp-4').addClass('animated pulse');
+    }, {
+            offset: '50%'
+        });
 });
